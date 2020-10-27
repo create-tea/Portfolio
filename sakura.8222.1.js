1c1ba67e56ef458fb78f4cefacf20e2be44b0f64
髙野晃二 <accelerator.k@gmail.com>
2020-10-27 11:22:26 +0900

    修正

--- sakura.8222.1.js
+++ sakura.8222.1.js
@@ -2,7 +2,7 @@
 	var q = d.createElement('div');
 	q.id = "sakura";
 	q.innerHTML = '<style>'+
-	'html,body{overflow-x:hidden;}'+
+	'html'+
 	'.hana{'+
 	'position:absolute;height:0;width:0;'+
 	'border: 10px solid pink;'+
